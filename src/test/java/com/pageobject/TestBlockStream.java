2e5054014195602f0c5bed30c9bb27aa3584d026
AnjaniQa <Anjani.kumar@digicert.com>
2025-03-13 16:35:40 +0530

    New commits

--- src/test/java/com/pageobject/TestBlockStream.java
+++ src/test/java/com/pageobject/TestBlockStream.java
@@ -1,7 +1,11 @@
 package com.pageobject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -11,7 +15,7 @@ public class TestBlockStream {
 
     @BeforeTest
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", "");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\AnjKumar\\Drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 
@@ -20,7 +24,11 @@ public class TestBlockStream {
     @Test
     public void validateTransactions() {
         driver.get("https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732");
-
+        WebElement transactionElement = driver.findElement(By.xpath("//h3[@class='font-h3']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true)", transactionElement);
+        String text = transactionElement.getText();
+        Assert.assertEquals("25 of 2875 Transactions", text);
     }
 
 }
